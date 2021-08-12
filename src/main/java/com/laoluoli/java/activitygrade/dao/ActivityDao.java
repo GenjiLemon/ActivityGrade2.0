17021b84ca1079d121c46710e5adf6c1b6d592ff
lemon <601749122@qq.com>
2021-08-12 17:23:37 +0800

    change activitydao to mapper_ xml

--- src/main/java/com/laoluoli/java/activitygrade/dao/ActivityDao.java
+++ src/main/java/com/laoluoli/java/activitygrade/dao/ActivityDao.java
@@ -8,32 +8,47 @@ import java.util.List;
 
 @Repository
 public interface ActivityDao {
-    @Select("select *  from activity")
     List<Activity> findAll();
 
-    //根据邀请码返回对应个体
-    @Select("select * from activity where invitationcode=#{code}")
+    /**
+     * 根据邀请码返回对应个体
+     * @param code 邀请码
+     * @return
+     */
     Activity findByCode(String code);
 
-    @Select("select *  from activity where id=#{id}")
+    /**
+     * 根据ID获取活动
+     * @param id 活动id
+     * @return
+     */
     Activity findById(Integer id);
 
-    //创建活动时的sql
-    @Insert("insert into activity (name,sponsor,brief,status,invitationcode,userid,begintime,maxjudge) " +
-            "values(#{name},#{sponsor},#{brief},#{status},#{invitationcode},#{userid},#{begintime},#{maxjudge})")
+    /**
+     * 新增一个活动
+     * @param activity
+     * @return 1成功，0失败
+     */
     int save(Activity activity);
 
-    //更新活动的sql
-    @Update("update activity set name=#{name},sponsor=#{sponsor},brief=#{brief},status=#{status}," +
-            "invitationcode=#{invitationcode},userid=#{userid},begintime=#{begintime} where id= #{id}")
+    /**
+     * 更新活动
+     * @param act 活动实例
+     * @return 1成功，0失败
+     */
     int update(Activity act);
 
-    //根据userid返回个体列表
-    @Select("select *  from activity where userid=#{userid}")
+    /**
+     * 根据userid返回活动列表
+     * @param userid 用户id
+     * @return 活动列表
+     */
     List<Activity> findActivityByUserid(Integer userid);
 
-    //结束活动时的sql
-    @Update("update activity set name=#{name},sponsor=#{sponsor},brief=#{brief},status=#{status}," +
-            "invitationcode=#{invitationcode},userid=#{userid},begintime=#{begintime},endtime=#{endtime} where id= #{id}")
+    /**
+     * 结束活动
+     * @param act 活动实例
+     * @return 1结束成功，0结束失败
+     */
     int updateWithEndtime(Activity act);
 }
---
+++ src/test/java/com/laoluoli/java/activitygrade/dao/ActivityDaoTests.java
@@ -0,0 +1,49 @@
+package com.laoluoli.java.activitygrade.dao;
+
+
+import com.laoluoli.java.activitygrade.domain.Activity;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.InputStream;
+import java.util.List;
+
+@SpringBootTest
+public class ActivityDaoTests {
+
+    @Autowired
+    ActivityDao activityDao;
+    @Test
+    public void testFindById() {
+        Activity activity = activityDao.findById(1);
+        System.out.println(activity);
+    }
+
+    @Test
+    public void testFindByCode() {
+        Activity activity = activityDao.findByCode("34dss");
+        System.out.println(activity);
+    }
+
+    @Test
+    public void testUpdate() {
+        Activity activity = activityDao.findById(1);
+        activity.setBrief("1对校园内的歌手参赛人员进行评分");
+        int res = activityDao.update(activity);
+        System.out.println(res);
+    }
+
+    @Test
+    public void testFindActivityByUserid() {
+       List<Activity> activities = activityDao.findActivityByUserid(1);
+        for(Activity e : activities){
+            System.out.println(e);
+        }
+    }
+}
