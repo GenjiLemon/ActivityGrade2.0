4b363a4861de356edb3805db9b8f8180461f8b80
lemon <601749122@qq.com>
2021-10-04 19:53:46 +0800

    Comments:Add JudgeDao.java comments

--- src/main/java/com/laoluoli/java/activitygrade/dao/JudgeDao.java
+++ src/main/java/com/laoluoli/java/activitygrade/dao/JudgeDao.java
@@ -9,18 +9,44 @@ import java.util.List;
 @Repository
 public interface JudgeDao {
 
-    //获取活动得评委list
+    /**
+     * 获取活动的评委列表
+     * @param activityid 活动id
+     * @return 评委list
+     */
     List<Judge> findListByActivityid(Integer activityid);
 
-    //获取用户得评委list
+
+    /**
+     * 获取用户担任过的所有评委
+     * @param userid 用户id
+     * @return 评委list
+     */
     List<Judge> findListByUserid(Integer userid);
 
-    //用户进入新比赛时创建评委
+
+    /**
+     * 新建评委
+     * @param judge 评委实体
+     * @return 成功为1，失败为0
+     */
     int save(Judge judge);
 
 
+    /**
+     * 根据id获取judge
+     * @param judgeid 评委id
+     * @return 评委实体
+     */
     Judge findById(Integer judgeid);
 
-    //获取指定用户在指定活动得个体
+    //获取指定用户在指定活动得
+
+    /**
+     * 获取用户在某活动的评委实体
+     * @param userid 用户id
+     * @param activityid 活动id
+     * @return 用户评委实体
+     */
     Judge findByUserandActivity(@Param("userid") Integer userid, @Param("activityid") Integer activityid);
 }
