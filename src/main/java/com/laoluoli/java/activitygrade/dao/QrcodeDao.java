df39d35c094225946ab715f748eea8bf2b7ec81a
lemon <601749122@qq.com>
2021-10-04 20:20:32 +0800

    Comments:Add Qrcode.java and UserDao.java comments

--- src/main/java/com/laoluoli/java/activitygrade/dao/QrcodeDao.java
+++ src/main/java/com/laoluoli/java/activitygrade/dao/QrcodeDao.java
@@ -6,7 +6,18 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QrcodeDao {
+    /**
+     * 获取某个活动的qrcode实体
+     * @param activityid 活动id
+     * @param type 二维码类型：1评委邀请二维码，2正常均值二维码，3截尾均值二维码
+     * @return Qrcode实体
+     */
     Qrcode findByActivityidType(@Param("activityid") Integer activityid, @Param("type") Integer type);
 
+    /**
+     * 新增一个二维码
+     * @param qrcode 二维码对象实体
+     * @return 1成功，0失败
+     */
     int Save(Qrcode qrcode);
 }
--- src/main/java/com/laoluoli/java/activitygrade/dao/UserDao.java
+++ src/main/java/com/laoluoli/java/activitygrade/dao/UserDao.java
@@ -5,12 +5,32 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserDao {
-    //常见的增删改查
+
+    /**
+     * 根据id获取用户
+     * @param id 用户id
+     * @return 用户实体
+     */
     User findById(Integer id);
 
+    /**
+     * 新增用户
+     * @param user 用户实体
+     * @return 1成功，0失败
+     */
     int save(User user);
 
+    /**
+     * 根据openid获取用户
+     * @param openid 微信openid
+     * @return 用户实体
+     */
     User findByOpenid(String openid);
 
+    /**
+     * 更新用户
+     * @param user 用户实体
+     * @return 1成功，0失败
+     */
     int update(User user);
 }
